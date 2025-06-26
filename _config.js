c880e3b0dc49b34093a34256389a10818d1bb80a
G-OMONDI <glenking194@gmail.com>
2025-06-26 22:54:14 +0300

    Connected to MongoDB Atlas

--- _config.js
+++ _config.js
@@ -5,5 +5,6 @@ config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    ip1: 'mongodb+srv://Glentest:glen123@ip1.jh2gvl1.mongodb.net/?retryWrites=true&w=majority&appName=IP1'
 }
 module.exports = config;
