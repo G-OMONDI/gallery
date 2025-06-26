30787c082ce93320012403723defec4f65da9b44
G-OMONDI <glenking194@gmail.com>
2025-06-27 00:23:14 +0300

    updated my _config.js file

--- _config.js
+++ _config.js
@@ -4,6 +4,7 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Glentest:glen123@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Glentest:glen123@cluster0.ttliynp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 }
 module.exports = config;
